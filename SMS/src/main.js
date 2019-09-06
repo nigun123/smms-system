e24066c20c8a0883801e46a33dd85e9714de7af8
nigun123 <592208416@qq.com>
2019-09-06 18:38:18 +0800

    页面修改

--- SMS/src/main.js
+++ SMS/src/main.js
@@ -1,9 +1,18 @@
+// 引入Vue组件
 import Vue from 'vue'
+// 引入ElementUI组件和样式
+import ElementUI from 'element-ui';            // 组件文件（js）
+import 'element-ui/lib/theme-chalk/index.css'; // 样式文件
+// 引入顶级组件 
 import App from './App.vue'
+// 引入路由文件
 import router from './router'
 
+// 注册elementui
+Vue.use(ElementUI);
+// 阻止生产提示
 Vue.config.productionTip = false
-
+// 创建vue实例对象  挂载dom  把路由和app顶级组件传入 渲染进入dom容器中
 new Vue({
   router,
   render: h => h(App)
